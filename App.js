e180db1ab4f7480706c66947f2810a6b7519b4f9
Mohamed-78 <dieudonneirie7@gmail.com>
2024-01-12 09:33:43 +0000

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { SegmentedSwitch } from './src/SegmentedSwitch';
 
 export default function App() {
+  const tabs = [
+    {
+      label: 'First',
+      content: (
+        <>
+          <View>
+            <Text>First</Text>
+          </View>
+        </>
+      ),
+    },
+    {
+      label: 'Second',
+      content: (
+        <>
+          <View>
+            <Text>Second</Text>
+          </View>
+        </>
+      ),
+    },
+    {
+      label: 'Third',
+      content: (
+        <>
+          <View>
+            <Text>Third</Text>
+          </View>
+        </>
+      ),
+    },
+  ];
+
+  const switchButtonStyle = {
+    activeColor: 'orange',
+    inactiveColor: 'transparent',
+    borderRadius: 10,
+    borderWidth: 0,
+    backgroundColor: 'lightblue',
+  };
+
+  const touchableStyle = {
+    activeColor: 'orange',
+    inactiveColor: 'transparent',
+    borderRadius: 20,
+  };
+
+  const textTouchableStyle = {
+    activeColor: '#fff',
+    inactiveColor: '#000',
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SegmentedSwitch
+        tabs={tabs}
+        switchButtonStyle={switchButtonStyle}
+        touchableStyle={touchableStyle}
+        textTouchableStyle={textTouchableStyle}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
